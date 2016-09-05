5998e01656004e825e7abcf2455d70ad7142a8a8
Karina Betzabe Romero Ulloa <betzy_skamm@hotmail.com>
2016-09-05 16:17:29 -0700

    Initial Commit

---
+++ btxLive-ios-app/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  btxLive-ios-app
+//
+//  Created by Karina Betzabe Romero Ulloa on 05/09/16.
+//  Copyright © 2016 Sandcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ btxLive-ios-app/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  btxLive-ios-app
+//
+//  Created by Karina Betzabe Romero Ulloa on 05/09/16.
+//  Copyright © 2016 Sandcode. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
